b596b982f6d1db67729b5d406a006a5c351ff3ba
inspiredgoat <tomd@airmail.cc>
2023-12-21 08:36:56 +1100

    now using thread safe getaddrinfo

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-gcc node.c -O0 -ltomcrypt -ltommath -DLTM_DESC && ./a.out
+tcc node.c -g -O0 -ltomcrypt -ltommath -DLTM_DESC && ./a.out
 # gcc /tmp/test.c -g -O0 -ltomcrypt -ltommath -DLTM_DESC && ./a.out
