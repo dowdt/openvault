7a9536bc8cba570efa7ec84370b192f9158d8e16
inspiredgoat <tomd@airmail.cc>
2023-12-20 19:52:23 +1100

    holy crap it actually works on half the sites

--- node.c
+++ node.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 #define WITH_TLS_13
-// #define DEBUG
+/* #define DEBUG */
 #include "tlse/tlse.c"
 #include "libhttp/http.c"
 
@@ -94,20 +94,25 @@ bool load_root_certificates(struct TLSContext* context)
 
 bool send_pending(Socket* sock, struct TLSContext* context)
 {
-    // send handhake
-    const unsigned char* buffer;
-    unsigned int left;
-
-    buffer = tls_get_write_buffer(context, &left);
-    if (buffer != NULL)
+    unsigned int out_buffer_len = 0;
+    const unsigned char *out_buffer = tls_get_write_buffer(context, &out_buffer_len);
+    unsigned int out_buffer_index = 0;
+    int send_res = 0;
+    while ((out_buffer) && (out_buffer_len > 0))
     {
-        // XXX: should try and send incrementally actually
-        printf("sending: %i/%i\n", net_send(sock, (void*) buffer, left), left);
+        int res = net_send(sock, (char *)&out_buffer[out_buffer_index], out_buffer_len);
+        if (res <= 0)
+        {
+            send_res = res;
+            break;
+        }
+
+        out_buffer_len -= res;
+        out_buffer_index += res;
     }
 
     tls_buffer_clear(context);
-
-    return left;
+    return send_res;
 }
 
 int verify_certificate(struct TLSContext *context, struct TLSCertificate **certificate_chain, int len)
@@ -115,30 +120,29 @@ int verify_certificate(struct TLSContext *context, struct TLSCertificate **certi
     return no_error;
 }
 
-int tls_recv(void* ctx, Socket* socket, void* buf, unsigned int len)
-{
-    int tls_ret;
-    int received_bytes;
-    struct TLSContext* context = (struct TLSContext*) ctx;
+/* int tls_recv(void* ctx, Socket* socket, void* buf, unsigned int len) */
+/* { */
+/*     int tls_ret; */
+/*     int received_bytes; */
+/*     struct TLSContext* context = (struct TLSContext*) ctx; */
 
-    memset(buf, 0, len);
-    received_bytes = net_recv(socket, buf, len);
+/*     memset(buf, 0, len); */
+/*     received_bytes = net_recv(socket, buf, len); */
 
-    if (received_bytes == -1)
-    {
-        return received_bytes;
-    }
+/*     if (received_bytes == -1) */
+/*     { */
+/*         return received_bytes; */
+/*     } */
 
-    // call tls
-    memset(buf, 0, len);
-    tls_read_clear(context);
-    tls_consume_stream(context, buf, received_bytes, verify_certificate);
+/*     // call tls */
+/*     memset(buf, 0, len); */
+/*     tls_consume_stream(context, buf, received_bytes, verify_certificate); */
 
-    // return your buf
-    tls_ret = tls_read(context, buf, len);
+/*     // return your buf */
+/*     tls_ret = tls_read(context, buf, len); */
 
-    return tls_ret;
-}
+/*     return tls_ret; */
+/* } */
 
 int main()
 {
@@ -167,7 +171,13 @@ int main()
 
     printf("loaded certificates\n");
 
-#define HOST "cedars.xyz"
+/* #define HOST "cedars.xyz" */
+#define HOST "www.wikipedia.org"
+/* #define HOST "www.google.com" */
+/* #define HOST "bestmotherfucking.website" */
+/* #define HOST "odin-lang.org" */
+/* #define HOST "www.slowernews.com" */
+/* #define HOST "stackoverflow.com" */
 
     // TODO: there is a random segfault here occasionally, could be bind or connect
     sock = net_connect(HOST, 443);
@@ -183,13 +193,15 @@ int main()
         int read_bytes;
         byte buf[4096];
         int sent_request = 0;
+        int n_loops = 0;
 
+        static struct http_message msg;
         while ((read_bytes = net_recv(sock, buf, 4096)) > 0)
         {
             printf("==>> READ: %i\n", read_bytes);
             // could be null validator
             tls_consume_stream(context, buf, read_bytes, NULL);
-            send_pending(sock, context); // bug here
+            /* send_pending(sock, context); // bug here */
 
             if (tls_established(context))
             {
@@ -207,25 +219,24 @@ int main()
                 /* while ((read_bytes = net_recv(sock, buf, 4096)) > 0) */
                 else
                 {
-                    static bool found_content = 0;
-#define DATA_SIZE 4096
+                    // NOTE: DATA_SIZE has to be smaller than http buffer, so that it fits entirely within one call to http_read_from_buf
+#define DATA_SIZE 1028
                     unsigned char data[DATA_SIZE];
+
                     printf("Reading\n");
-                    tls_read_clear(context);
-                    tls_consume_stream(context, buf, read_bytes, verify_certificate);
-                    send_pending(sock, context);
 
                     memset(data, 0, DATA_SIZE);
 
                     int ret;
                     while ((ret = tls_read(context, data, DATA_SIZE)) > 0)
                     {
-                        printf("Got decrypted text\n");
-                        static struct http_message msg;
+                        printf("Got decrypted text size: %i\n", ret);
 
+                        // what happens if http reads but has nothing to write?
                         int h = http_read_from_buf(data, ret, &msg);
+                        n_loops += 1;
 
-                        printf("ret %i, len %i\n", h, msg.length);
+                        printf("ret: %i, len: %i, total: %i, header length: %i\n", h, msg.length, msg.state.total, msg.header.length);
 
                         if (msg.length > 0)
                         {
@@ -234,10 +245,11 @@ int main()
                         }
                         printf("h val: %i\n", h);
 
-                        if (strstr(msg.content, "</html>") != NULL)
-                        {
-                            int a = 2;
-                        }
+                        /* if (strstr(msg.content, "</html>") != NULL) */
+                        /* { */
+                        /*     int a = 2; */
+                        /*     printf("saw end of html tag\n"); */
+                        /* } */
 
                         if (msg.header.length == msg.state.total && msg.state.left == 0)
                         {
@@ -262,9 +274,6 @@ int main()
             }
         }
     }
-
-    printf("send no prob!!\n");
-    
 
     return 0;
 }
